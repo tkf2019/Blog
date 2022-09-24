dc0b9ad6c5c79861601345d8ce9db81a9c2cc211
田凯夫 <tkf19@mails.tsinghua.edu.cn>
2022-09-24 10:03:16 +0800

    Test options

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'TKF\'s Blog'
+project = 'TKF\'s Daily Notes'
 copyright = '2022, TKF'
 author = 'TKF'
 
@@ -25,3 +25,15 @@ language = 'zh'
 
 html_theme = 'furo'
 html_static_path = ['_static']
+html_theme_options = {
+    "sidebar_hide_name": True,
+
+    # Adding an edit button
+    "source_repository": "https://github.com/tkf2019/Blog",
+    "source_branch": "main",
+    "source_directory": "source/",
+}
+
+# Custom options for Furo
+
+# html_logo = '_static/logo.jpeg'
